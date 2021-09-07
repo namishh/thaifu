f9826128fd072165c7fd8d563ed65cc89c771b7a
Namish Pande <namishpande2007@gmail.com>
2021-09-07 21:42:59 +0530

    menu

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// Navigation
 const navToggle = document.querySelector('.navbar-toggle-wrapper')
 const navBtn = document.querySelector('.navbar-toggle')
 const navbar = document.querySelector('.navbar')
@@ -5,4 +6,22 @@ const navbar = document.querySelector('.navbar')
 navToggle.addEventListener('click', () => {
   navbar.classList.toggle('navbar-visible')
   navBtn.classList.toggle('navbar-toggle-active')
+})
+
+// Menu
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget)
+    tabContents.forEach(el => {
+      el.classList.remove("menu-food-active")
+    })
+    tabs.forEach(el => {
+      el.classList.remove("menu-item-active")
+    })
+    tab.classList.add('menu-item-active')
+    target.classList.add("menu-food-active")
+  })
 })
