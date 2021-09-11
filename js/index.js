2dab96262437555705fb11cee38d6b9777c55fe7
Namish Pande <namishpande2007@gmail.com>
2021-09-11 23:04:49 +0530

    removed sticky nav

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 const navToggle = document.querySelector('.navbar-toggle-wrapper')
 const navBtn = document.querySelector('.navbar-toggle')
 const navbar = document.querySelector('.navbar')
-
 navToggle.addEventListener('click', () => {
   navbar.classList.toggle('navbar-visible')
   navBtn.classList.toggle('navbar-toggle-active')
@@ -104,22 +103,32 @@ sections.forEach(function(section) {
   section.classList.add('section--hidden')
 })
 
-// Sticky Navbar
-const hero = sections[0]
-const heroObserver = new IntersectionObserver((entries, observer) => {
-  const [entry] = entries
-  if(!entry.isIntersecting){
-     navbar.classList.add('navbar-sticky')
-  }else{
-    navbar.classList.remove('navbar-sticky')
-  }
-}, {
-  root:null,
-  threshold : .2 
-})
-heroObserver.observe(hero)
 
 // Intro Animation
 const timeline = gsap.timeline({defaults : {ease : "power1.out"}})
 timeline.to('.intro-text', {y : '0%', duration : 0.8, stagger : 0.25, delay : 0.2})
 timeline.to('.intro', {y : '-100%', duration : 0.8, stagger : 0.25, delay:1})
+
+// Smooth Scroll
+const links = document.querySelectorAll('[data-link]')
+const linkList = [links]
+const addSmoothScroll = (linkList) => {
+  linkList.forEach(list => {
+    list.forEach(link => {
+      link.addEventListener('click', (e) => {
+        e.preventDefault()
+        navbar.classList.add('navbar-sticky')
+        const section = document.querySelector(`.${link.dataset.link}`)
+        const coordinates = section.getBoundingClientRect()
+        window.scrollTo({
+            left : coordinates.left + window.pageXOffset, 
+            top : coordinates.top + window.pageYOffset - 30,
+            // Adding behavior
+            behavior : "smooth"
+        })
+      })
+    })
+  })
+}
+
+addSmoothScroll(linkList)
